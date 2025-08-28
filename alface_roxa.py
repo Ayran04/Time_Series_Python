a5e35833cb2ac1b5b45b86efbe048d12c0ce1864
AYRAN DHROEL BADE <163210281+Ayran04@users.noreply.github.com>
2025-08-28 19:29:31 -0300

    Update alface_roxa.py
    Resposta da tarefa

--- alface_roxa.py
+++ alface_roxa.py
@@ -2,4 +2,17 @@
 
 #### QUAL A DIFERENÇA ENTRE UM CABEÇALHO, UM COMENTÁRIO OU UMA DOCSTRING EM PYTHON?
 
+Resposta: 
 
+CABEÇALHO
+Bloco de informações que ficam no início do código,
+que serve pra identificar o arquivo, autor, data, versão, entre outros.
+
+COMENTÁRIOS
+São textos que podem estar em qualquer lugar do código, usados para explicar o que 
+está acontecendo algum trecho do código. Ele vai ser ignorado quando rodar o código Começa com #
+
+DOCSSTRINGS
+É uma string especial, ela é usada para documentar módulos, classes, funções e métodos. 
+Ela fica armazenada dentro do objeto e dá para acessar enquanto o programa está rodando. 
+É escrita entre três aspas duplas ou simples (Exemplo: """ teste """).
